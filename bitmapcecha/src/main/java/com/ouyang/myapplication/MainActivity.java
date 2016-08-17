a35cfc82d9a4684a376620852257130ab5905db3
hongshulin1605 <1172990087@qq.com>
2016-08-17 16:39:39 +0800

    log  22222

--- bitmapcecha/src/main/java/com/ouyang/myapplication/MainActivity.java
+++ bitmapcecha/src/main/java/com/ouyang/myapplication/MainActivity.java
@@ -2,14 +2,6 @@ package com.ouyang.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ListView;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,40 +10,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ListView lv = (ListView) findViewById(R.id.lv);
-        List<String> list = new ArrayList<>();
 
-        for (int i = 0; i < 45; i++) {
-            list.add(">>>>>>>>>>>>" + i);
-        }
-
-        MyAdapter adapter = new MyAdapter(this,list);
-        lv.setAdapter(adapter);
-
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-        Log.e("print", "onCreate: " + 22222);
-
-        TextView tv;
-        ImageView iv;
-        Button btn;
 
     }
-
-
-
 
 }
